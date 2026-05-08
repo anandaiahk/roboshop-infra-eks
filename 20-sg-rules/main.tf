147a6ccb2cf106fc44cd158f5fd184bf27cca14c
anand k <kanandaiah9@gmail.com>
2026-05-08 13:10:26 +0530

    k8

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -1,53 +1,58 @@
+# Bastion
 resource "aws_security_group_rule" "bastion_internet" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  #cidr_blocks       = ["0.0.0.0/0"]
   cidr_blocks       = [local.my_ip]
-  # give bastion security group
+  # which SG you are creating this rule
   security_group_id = local.bastion_sg_id
 }
 
+# MongoDB
 resource "aws_security_group_rule" "mongodb_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  # Where traffic is coming from
   source_security_group_id = local.bastion_sg_id
-  # give bastion security group
   security_group_id = local.mongodb_sg_id
 }
 
-
-
-
+# Redis
 resource "aws_security_group_rule" "redis_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  # Where traffic is coming from
   source_security_group_id = local.bastion_sg_id
-  # give bastion security group
   security_group_id = local.redis_sg_id
 }
+
+# MySQL
 resource "aws_security_group_rule" "mysql_bastion" {
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
+  # Where traffic is coming from
   source_security_group_id = local.bastion_sg_id
-  # give bastion security group
   security_group_id = local.mysql_sg_id
 }
+
 resource "aws_security_group_rule" "mysql_eks_node" {
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
+  # Where traffic is coming from
   source_security_group_id = local.eks_node_sg_id
-  # give bastion security group
   security_group_id = local.mysql_sg_id
 }
+
 # RabbitMQ
 resource "aws_security_group_rule" "rabbitmq_bastion" {
   type              = "ingress"
@@ -59,7 +64,6 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
   security_group_id = local.rabbitmq_sg_id
 }
 
-# Frontend ALB
 resource "aws_security_group_rule" "ingress_alb_public" {
   type              = "ingress"
   from_port         = 443
@@ -92,7 +96,6 @@ resource "aws_security_group_rule" "openvpn_public_943" {
   security_group_id = local.openvpn_sg_id
 }
 
-# Admin UI
 resource "aws_security_group_rule" "eks_control_plane_bastion" {
   type              = "ingress"
   from_port         = 443
@@ -103,7 +106,6 @@ resource "aws_security_group_rule" "eks_control_plane_bastion" {
   security_group_id = local.eks_control_plane_sg_id
 }
 
-# Admin UI
 resource "aws_security_group_rule" "eks_node_bastion" {
   type              = "ingress"
   from_port         = 22
@@ -114,24 +116,32 @@ resource "aws_security_group_rule" "eks_node_bastion" {
   security_group_id = local.eks_node_sg_id
 }
 
-# Admin UI
+resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # all traffic
+  # Where traffic is coming from
+  source_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+}
+
 resource "aws_security_group_rule" "eks_node_eks_control_plane" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
-  protocol          = "-1"
+  protocol          = "-1" # all traffic
   # Where traffic is coming from
   source_security_group_id = local.eks_control_plane_sg_id
   security_group_id = local.eks_node_sg_id
 }
 
-# Admin UI
 resource "aws_security_group_rule" "eks_node_vpc_cidr" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
-  protocol          = "-1"
-  # Where traffic is coming from
-   cidr_blocks = ["10.0.0.0/16"]
-  security_group_id = local.eks_control_plane_sg_id
+  protocol          = "-1" # all traffic
+  # VPC CIDR
+  cidr_blocks = ["10.0.0.0/16"]
+  security_group_id = local.eks_node_sg_id
 }
