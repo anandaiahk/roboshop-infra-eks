4e4843d4e7dedc795a16a9d7e034bb7e4d5e7991
anand k <kanandaiah9@gmail.com>
2026-05-09 08:59:44 +0530

    k8

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -4,7 +4,6 @@ resource "aws_security_group_rule" "bastion_internet" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  #cidr_blocks       = ["0.0.0.0/0"]
   cidr_blocks       = [local.my_ip]
   # which SG you are creating this rule
   security_group_id = local.bastion_sg_id
