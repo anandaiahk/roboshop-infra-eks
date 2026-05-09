2d142eaaee9937dd2111187306c0d7982fed5888
anand k <kanandaiah9@gmail.com>
2026-05-09 09:00:07 +0530

    k8

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0243750UF68Z7UBMQ4E"
+}
+
+variable "domain_name" {
+    default = "anand88b.online"
+}
