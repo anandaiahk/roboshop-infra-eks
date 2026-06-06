496c3e00afa805f73fc1463c634be1857b10fc33
anand k <kanandaiah9@gmail.com>
2026-06-06 19:21:28 +0530

    jenkins

---
+++ 31-cicd/variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default =  "Z0243750UF68Z7UBMQ4E"
+
+}
+
+variable "domain_name" {
+    default = "anand88b.online"
+}
+
+variable "sonar" {
+    default = true
+}
+
+variable "jenkins" {
+    default = true
+}
+
+variable "runner" {
+    default = false
+}
