b850d3c6c954a03d38eb17512b1afa139e38397c
anand k <kanandaiah9@gmail.com>
2026-06-06 19:57:00 +0530

    jenkins

---
+++ 31-cicd/runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+growpart /dev/nvme0n1 4
+lvextend -L +10G /dev/mapper/RootVG-rootVol
+lvextend -L +10G /dev/mapper/RootVG-homeVol
+lvextend -L +10G /dev/mapper/RootVG-varVol
+
+xfs_growfs /var
+xfs_growfs /home
+xfs_growfs /
