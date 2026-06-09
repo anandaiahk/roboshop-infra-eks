88cfa08650113e67290b038e06298b307d353021
anand k <kanandaiah9@gmail.com>
2026-06-09 22:20:25 +0530

    jenkins

--- 31-cicd/variables.tf
+++ 31-cicd/variables.tf
@@ -14,10 +14,9 @@ variable "zone_id" {
 variable "domain_name" {
     default = "anand88b.online"
 }
-
-/* variable "sonar" {
+variable "sonar" {
     default = true
-} */
+} 
 variable "jenkins" {
     default = true
 }
