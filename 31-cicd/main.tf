63ca5d0ac418fe23f3e0d1328e3df538c7612d1c
anand k <kanandaiah9@gmail.com>
2026-06-10 21:47:06 +0530

    jenkins

--- 31-cicd/main.tf
+++ 31-cicd/main.tf
@@ -86,7 +86,7 @@ resource "aws_instance" "sonarqube" {
   instance_type = "t3.large"
   vpc_security_group_ids = [local.sonar_sg_id]
   subnet_id = local.public_subnet_id #replace your Subnet in default VPC
-  key_name = "anand-88s"
+  
   # need more for terraform
   root_block_device {
     volume_size = 20
