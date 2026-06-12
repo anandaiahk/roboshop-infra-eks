7807b44d242bb6e908937eb685eaca69b40a685d
anand k <kanandaiah9@gmail.com>
2026-06-12 07:49:06 +0530

    jenkins

--- 31-cicd/data.tf
+++ 31-cicd/data.tf
@@ -17,6 +17,25 @@ data "aws_ami" "joindevops" {
     values = ["hvm"]
   }
 }
+data "aws_ami" "sonarqube" {
+  most_recent = true
+  owners      = ["679593333241"] # Solve DevOps
+
+  filter {
+    name   = "name"
+    values = ["SolveDevOps-SonarQube-Server-Ubuntu24.04-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 
 
 
@@ -31,7 +50,9 @@ data "aws_ssm_parameter" "jenkins_sg_id" {
 data "aws_ssm_parameter" "jenkins_agent_sg_id" {
     name = "/${var.project}/${var.environment}/jenkins_agent_sg_id"
 }
-
+data "aws_ssm_parameter" "sonar_sg_id" {
+    name = "/${var.project}/${var.environment}/sonar_sg_id"
+}
 
 data "aws_ssm_parameter" "runner_sg_id" {
     name = "/${var.project}/${var.environment}/runner_sg_id"
