aa3edd6bba5f36bfe6bdab1ed82b3de356fd9d43
anand k <kanandaiah9@gmail.com>
2026-06-12 06:24:38 +0530

    jenkins

--- 31-cicd/data.tf
+++ 31-cicd/data.tf
@@ -18,20 +18,7 @@ data "aws_ami" "joindevops" {
   }
 }
 
-data "aws_ami" "sonarqube" {
-  most_recent = true
-  owners      = ["099720109477"]  # Canonical - Free, No Marketplace
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 
 data "aws_ssm_parameter" "public_subnet_ids" {
     name = "/${var.project}/${var.environment}/public_subnet_ids"
