012e70e682431630815a126d0018fbe164350fd1
anand k <kanandaiah9@gmail.com>
2026-06-12 06:23:24 +0530

    jenkins

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -46,7 +46,7 @@ data "aws_ssm_parameter" "jenkins_sg_id" {
 data "aws_ssm_parameter" "jenkins_agent_sg_id" {
     name = "/${var.project}/${var.environment}/jenkins_agent_sg_id"
 }
-x 
+
 
 data "aws_ssm_parameter" "runner_sg_id" {
     name = "/${var.project}/${var.environment}/runner_sg_id"
