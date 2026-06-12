1250a569fecf8b71b24271d48f8d590f3f128d1f
anand k <kanandaiah9@gmail.com>
2026-06-12 06:22:05 +0530

    jenkins

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -220,22 +220,3 @@ resource "aws_security_group_rule" "jenkins_agent_ssh" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = local.jenkins_agent_sg_id
 }
-resource "aws_security_group_rule" "sonar_web" {
-  type              = "ingress"
-  from_port         = 9000
-  to_port           = 9000
-  protocol          = "tcp" # all traffic
-  # VPC CIDR
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = local.sonar_sg_id
-}
-
-resource "aws_security_group_rule" "sonar_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp" # all traffic
-  # VPC CIDR
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = local.sonar_sg_id
-} 
